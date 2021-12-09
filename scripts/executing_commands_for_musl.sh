c61d6e304fd596e993b668c3307a478e2bd97165
p0isk <radio.86rk@gmail.com>
2021-12-10 01:52:39 +0300

    Add GITHUB_VERSION

--- scripts/executing_commands_for_musl.sh
+++ scripts/executing_commands_for_musl.sh
@@ -3,7 +3,8 @@
 # Please check BR2_ROOTFS_POST_BUILD_SCRIPT option in config
 #
 
-date "+OPENIPC_VERSION=2.1.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
+GITHUB_VERSION="${GIT_LOCAL_BRANCH}+${GIT_REVISION,length=7}"
+date "+OPENIPC_VERSION=${GITHUB_VERSION}, 2.1.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
 #
 echo 'Note: BR2_TOOLCHAIN_BUILDROOT_LIBC="musl"'
 #
