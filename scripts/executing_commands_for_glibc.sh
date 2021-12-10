2cfff11bb559ee0156c2bbe166a7cb138c7061bb
p0isk <radio.86rk@gmail.com>
2021-12-10 09:25:59 +0300

    Add GITHUB_VERSION

--- scripts/executing_commands_for_glibc.sh
+++ scripts/executing_commands_for_glibc.sh
@@ -3,8 +3,9 @@
 # Please check BR2_ROOTFS_POST_BUILD_SCRIPT option in config
 #
 
-GITHUB_VERSION="${GIT_LOCAL_BRANCH}+${GIT_REVISION,length=7}"
-date "+OPENIPC_VERSION=${GITHUB_VERSION}, 2.1.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
+GITHUB_VERSION="${BRANCH_NAME}+${GIT_HASH}" 
+RELEASE_DATE=$(date "+%y.%m.%d") 
+echo "OPENIPC_VERSION=${GITHUB_VERSION}, ${RELEASE_DATE:0:1}.${RELEASE_DATE:1}" >>${TARGET_DIR}/usr/lib/os-release
 #
 echo 'Note: BR2_TOOLCHAIN_BUILDROOT_LIBC="glibc"'
 #
